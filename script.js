091769ca5be7144ba0794e77eda3af412d5da592
Blladerunner <rufatahmedik@gmail.com>
2024-02-25 19:29:11 +0100

    hamburger menu drop down menu implementation

--- script.js
+++ script.js
@@ -1,12 +1,18 @@
 const btnHamburger = document.querySelector("#btnHamburger");
 const hero = document.querySelector(".hero");
+const overlay = document.querySelector(".overlay");
 
 btnHamburger.addEventListener("click", function () {
   console.log("click hamburger");
 
   if (hero.classList.contains("open")) {
+    // Close hamburger menu
     hero.classList.remove("open");
+    overlay.classList.remove("fade-in");
+    overlay.classList.add("fade-out");
   } else {
-    hero.classList.add("open");
+    hero.classList.add("open"); // Open hamburger menu
+    overlay.classList.remove("fade-out");
+    overlay.classList.add("fade-in");
   }
 });
